8ce2199b6c25a75b5190114815cede40c5517571
yuanb <billyuan@gmail.com>
2019-02-15 21:36:57 -0500

    added header, data payload dump and checksum calculation.

--- src/dd20emu.c
+++ src/dd20emu.c
@@ -20,7 +20,7 @@
 
 #define TRKSIZE_FM      3172    /* size of a standard FM mode track */
 
-typedef struct Headers {
+typedef struct SectorHeaders {
 	uint8_t		GAP1[7];
 	uint8_t		IDAM_leading[4];
 	uint8_t		TR;
@@ -28,13 +28,13 @@ typedef struct Headers {
 	uint8_t		TS_sum;
 	uint8_t		GAP2[6];
 	uint8_t		IDAM_closing[4];
-} header_t;
+} sec_hdr_t;
 
 typedef struct Sectors {
-	header_t	header;
+	sec_hdr_t	header;
 	uint8_t		data_bytes[126];
 	uint8_t		next_track;
-	uint8_t 	next_sector;
+	uint8_t		next_sector;
 	uint16_t	checksum;
 } sector_t;
 
@@ -59,14 +59,71 @@ int get_track(FILE *fp, int n)
 	r = fread(&fdc_data, size, 1, fp);
 	printf("T:%02d, size=%d\r\n", n, size);
 
-	printf("Header dump:\r\n");
-	for(int i=0; i<3; i++)
+	for(int i=0; i<SEC_NUM; i++)
 	{
-		for(int j=0; j<8; j++)
+		sector_t *sec = (sector_t *)&fdc_data[i* sizeof(sector_t)];
+		sec_hdr_t *hdr = &sec->header;
+
+
+		printf("Sector # %02x", i);
+		printf("\r\ngap1: ");
+		for(int j=0; j<sizeof(hdr->GAP1); j++)
 		{
-			printf("%02X ", fdc_data[i*8+j]);
+			printf("%02X ", hdr->GAP1[j]);
 		}
-		printf("\r\n");
+
+		printf("\r\nidam_leading: ");
+		for(int j=0; j<sizeof(hdr->IDAM_leading); j++)
+		{
+			printf("%02X ", hdr->IDAM_leading[j]);
+		}
+
+		printf("\r\nTR: %02X, SC: %02X, T+S: %02X", hdr->TR, hdr->SC, hdr->TS_sum);
+
+		printf("\r\ngap2: ");
+		for(int j=0; j<sizeof(hdr->GAP2); j++)
+		{
+			printf("%02X ", hdr->GAP2[j]);
+		}
+
+
+		printf("\r\nIDAM_closing: ");
+		for(int j=0; j<sizeof(hdr->IDAM_closing); j++)
+		{
+			printf("%02X ", hdr->IDAM_closing[j]);
+		}
+
+		printf("\r\ndata:\r\n");
+		for(int j=0; j<sizeof(sec->data_bytes); j++)
+		{
+			printf("%02X ", sec->data_bytes[j]);
+		}
+
+		if (n!=0)
+		{
+			printf("\r\nNext TR: %02X, SC: %02X", sec->next_track, sec->next_sector);
+		}
+		else
+		{
+			printf("%02x %02X", sec->next_track, sec->next_sector);
+		}
+
+		uint16_t checksum = 0;
+		for(int j=0; j<sizeof(sec->data_bytes); j++)
+		{
+			checksum += sec->data_bytes[j];
+		}
+		checksum += sec->next_track;
+		checksum += sec->next_sector;
+		if (sec->checksum != checksum)
+		{
+			printf("\r\nCHECKSUM ERROR");
+			exit(-1);
+		}
+
+		printf("\r\nVerified checksum=: %04X", checksum);
+
+		printf("\r\n\r\n");
 	}
 
 	return 0;
@@ -87,11 +144,13 @@ int main ()
 		exit(-1);
 	}
 
-	for(int i=0; i<40; i++)
+	for(int i=0; i<TRK_NUM; i++)
 	{
 		get_track(fp, i);
 	}
 
+
+	printf("End of disk dump\r\n");
 	fclose(fp);
 	exit(0);
 }
