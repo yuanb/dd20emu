44e43f8563701fc59458caa2f8d62650c8cb7f36
yuanb <yuanb@users.noreply.github.com>
2021-02-23 13:56:03 -0500

    Added license header (#17)

--- dd20emu/dd20emu.ino
+++ dd20emu/dd20emu.ino
@@ -1,9 +1,19 @@
-
 /*
-   DD-20 emulator
-   Author: Bill Yow
-   https://github.com/yuanb/dd20emu
+    DD-20 emulator
+    Copyright (C) 2020,2021 https://github.com/yuanb/
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 /* Libraries used:
@@ -12,10 +22,12 @@
  */
 
 /*
- * TODO: CLI interface
+ * TODO: 
+ * 1. CLI interface
  *        https://www.norwegiancreations.com/2018/02/creating-a-command-line-interface-in-arduinos-serial-monitor/
  * 
  */
+
 #include <SPI.h>
 #include "SdFat.h"
 #include "PinChangeInterrupt.h"
