1ac8cdbe238345a63b555d2147a75f9f0f38e1e0
yuanb <yuanb@users.noreply.github.com>
2023-01-22 13:49:25 -0500

    added assert support

--- dd20emu/utilities.ino
+++ dd20emu/utilities.ino
@@ -71,3 +71,15 @@ int freeMemory() {
   return __brkval ? &top - __brkval : &top - __malloc_heap_start;
 #endif  // __arm__
 }
+
+// handle diagnostic informations given by assertion and abort program execution:
+void __assert(const char *__func, const char *__file, int __lineno, const char *__sexp) {
+    // transmit diagnostic informations through serial link. 
+    Serial.println(__func);
+    Serial.println(__file);
+    Serial.println(__lineno, DEC);
+    Serial.println(__sexp);
+    Serial.flush();
+    // abort program execution.
+    abort();
+}
