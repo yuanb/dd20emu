f4d7f90394e354f3ec710aab0491af5afa98f0c9
yuanb <yuanb@users.noreply.github.com>
2023-02-10 17:29:22 -0500

    Update utilities.ino

--- dd20emu/utilities.ino
+++ dd20emu/utilities.ino
@@ -22,10 +22,9 @@
  * LED visual when Drive is enabled
  * Low is enabled, power led when main unit is turned off
  */
-bool old_drv_enabled = -1; 
+
 inline void handle_drive_enable() {
-  digitalWrite(wrProtPin, drv_enabled);
-    
+  static bool old_drv_enabled = -1;
   if (drv_enabled != old_drv_enabled) {
     digitalWrite(LED_BUILTIN, drv_enabled);
     old_drv_enabled = drv_enabled;
